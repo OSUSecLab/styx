de87333d2f788bb4f2aad4984d32efaa82b44241
NSKernel <zhao.3289@osu.edu>
2023-11-26 15:18:55 -0500

    Refactored SHA256 SGX backedn

--- platforms/sgx/include/crypto/sha256_backend.h
+++ platforms/sgx/include/crypto/sha256_backend.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-typedef uint8_t pcd_sha256_t[32];
+#include "crypto/sha256.h"
 
 int pcd_crypto_backend_sha256_hash_buffer(uint8_t *buffer, size_t size, pcd_sha256_t *output_hash);
 
