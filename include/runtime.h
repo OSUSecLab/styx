afc1015cd4d0a720c1fef03d68abbba51bb39ebc
NSKernel <zhao.3289@osu.edu>
2023-11-26 15:17:42 -0500

    Exported app to native pointer

--- include/runtime.h
+++ include/runtime.h
@@ -21,6 +21,7 @@ void pcd_runtime_destroy_environment(void);
 pcd_runtime_pointer_t pcd_runtime_malloc(pcd_instance_t *instance, const uint32_t data_size, void **native_ptr);
 pcd_runtime_pointer_t pcd_runtime_copy_data_into_runtime(pcd_instance_t *instance, const void *data, const uint32_t data_size);
 void *pcd_runtime_app_to_native(pcd_instance_t *instance, pcd_runtime_pointer_t app_addr);
+pcd_runtime_pointer_t pcd_runtime_native_to_app(pcd_instance_t *instance, void *native_addr);
 void pcd_runtime_free(pcd_instance_t *instance, pcd_runtime_pointer_t app_addr);
 
 #endif
