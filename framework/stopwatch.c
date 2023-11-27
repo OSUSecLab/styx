50710ce07e1192b9624c4a0b265430adf7b97f4e
NSKernel <zhao.3289@osu.edu>
2023-11-27 11:03:36 -0500

    Changed stopwatch text

--- framework/stopwatch.c
+++ framework/stopwatch.c
@@ -40,7 +40,7 @@ void pcd_eval_stopwatch_lap(char *label, uint64_t *watch, char print) {
 	{
 		new_time = time_get();
 		if (print)
-			printf("time elapsed at %s is %llu\n", label, new_time - *watch);
+			printf("%s took %llu\n", label, new_time - *watch);
 		*watch = time_get();
 	}
 }
